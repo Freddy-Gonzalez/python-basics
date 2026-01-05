9237da7b618ecb2104aa5277cd00328925a3a06b
Freddy Gonzalez <freddyg.cafe@gmail.com>
2026-01-05 18:41:03 -0300

    Ejercicio 03: Calculadora de cambio

---
+++ Ejercicios-hola-mundo/Ejercicio-03.py
@@ -0,0 +1,5 @@
+monto_abonado = float(input("Ingrese el monto abonado: "))
+precio_producto = float(input("Ingrese precio del produco: "))
+
+cambio = monto_abonado - precio_producto
+print(f"Cambio: {cambio:.2f}")
