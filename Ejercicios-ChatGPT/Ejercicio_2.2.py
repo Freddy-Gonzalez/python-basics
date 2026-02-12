aa71324365131ebbaa77878df05342192e97d5f4
Freddy Gonzalez <freddyg.cafe@gmail.com>
2026-02-12 20:25:25 -0300

    Conversor numérico flexible

---
+++ Ejercicios-ChatGPT/Ejercicio_2.2.py
@@ -0,0 +1,11 @@
+n1 = float(input("Ingrese un numero: "))
+
+
+doble = n1 * 2
+mitad = n1 / 2
+bl = bool(n1)
+
+print(f"El numero ingresado es: {n1}")
+print(f"El doble es: {doble}")
+print(f"La mitad es: {mitad}")
+print(f"Es verdadero?: {bl}")
