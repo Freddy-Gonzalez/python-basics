f9da6c138a57cc58a616590a924b2d930f76d79b
Freddy Gonzalez <freddyg.cafe@gmail.com>
2026-02-19 21:32:10 -0300

    Datos con limpieza de texto y salto de línea

---
+++ Ejercicios-ChatGPT/Ejercicio_2.4.py
@@ -0,0 +1,5 @@
+nom = input("Ingrese su nombre: ").strip().title()
+apell = input("Ingrese su apellido: ").strip().title()
+prof = input("Ingrese su profesión: ").strip().title()
+
+print(f"Datos: \nNombre: {nom} \nApellido: {apell} \nProfesión: {prof}")
