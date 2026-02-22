5f8633ca05ccf5a538f124249040b6f47b7f74e2
Freddy Gonzalez <freddyg.cafe@gmail.com>
2026-02-22 19:51:50 -0300

    Ejercicio 4 usando join()

---
+++ Ejercicios-ChatGPT/Ejercicio_2.10.py
@@ -0,0 +1,7 @@
+producto1 = "Espresso"
+producto2 = "Cappuccino"
+producto3 = "Flat White"
+partes = [producto1, producto2, producto3]
+menu_del_dia = " | ".join(partes)
+
+print(f"Menú del día: {menu_del_dia}")
